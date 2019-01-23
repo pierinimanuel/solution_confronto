515ece79f42dea4e1763ac0bf2e7de3428b96132
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-23 09:18:22 +0100

    primo test green

---
+++ UnitTestProject1/Confronto_Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Confronto_Tests
+    {
+        [TestMethod]
+        public void TestMax()
+        {
+            int n1 = 0;
+            int n2 = 7;
+            int max_aspettato = 7;
+            int max_calcolato = confronto_library.Confronto.Max(n1, n2);
+            Assert.AreEqual(max_aspettato, max_calcolato);
+
+        }
+        
+
+    }
+}
---
+++ confronto_library/Confronto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace confronto_library
+{
+    public class Confronto
+    {
+        public static int Max(int n1, int n2)
+        {
+            if (n1 > n2)
+            {
+                return n1;
+            }
+            else
+            {
+                return n2;
+            }
+        }
+
+       
+    }
+}
